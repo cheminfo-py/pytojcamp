81e550f87267c6f81083097d51270ae63de53809
Kevin <kevin.jablonka@epfl.ch>
2020-09-29 10:26:53 +0200

    fix: versioneer not properly configured

--- pytojcamp/__init__.py
+++ pytojcamp/__init__.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 """pytojcamp package to write JCAMP files from Python"""
+from ._version import get_versions
 from .from_xy import from_xy
+
+__version__ = get_versions()["version"]
+del get_versions
